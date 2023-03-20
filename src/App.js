62ebbd64a32bccf5d99b6958f418cd74ba186196
dsx1997 <threebigstar@outlook.com>
2023-03-20 12:10:30 +0900

    complete game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -9,48 +8,31 @@ function App() {
       
       <header className="App-header">
         <Game />
-           <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>     
       </header>      
       
     </div>
   );
 }
 
-class Square extends React.Component {
-  
-  render() {
+function Square(props) {
     return (
       
-      <button className="square" >
-        
+      <button className="square" onClick={props.funcProps1}>
+        {props.valProps1}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
 
   renderSquare(i) {
-    return <Square />;
+    return <Square valProps1={this.props.valProps2[i]} funcProps1={() => this.props.funcProps2(i)}/>;
   }
 
   render() {
-    const status = 'Next player: X';
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -72,21 +54,107 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history : [{
+        squares : Array(9).fill(null),
+        // squares : [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      }],
+      xIsNext : true, 
+      stepNumber : 0,
+    };
+  }
+
+  handleClick(i) {
+    console.log('handleClick : ', i);
+    let history = this.state.history.slice(0, this.state.stepNumber + 1);
+    let current = history[history.length - 1];
+    let squares = current.squares.slice();
+    if(squares[i] || judgeWinner(squares)) {
+      console.log('return case');
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history : history.concat([{
+        squares : squares,
+      }]),
+      xIsNext : !this.state.xIsNext,
+      stepNumber : history.length,
+    });
+  }
+
+  jumpTo(move) {
+    console.log('jumpTo : ', move);
+    this.setState({
+      stepNumber : move,
+      xIsNext : move % 2 === 0,
+    });
+  }
+
+  render() {    
+
+    let stepNumber = this.state.stepNumber;
+    console.log('stepNumber : ', stepNumber);
+    let history = this.state.history.slice(0, stepNumber + 1);
+    console.log(history);
+    let current = history[history.length - 1];
+    console.log(current);
+    let squares = current.squares.slice();
+    console.log(squares);
+
+    let status;
+    let winner = judgeWinner(squares);
+    if(winner) {
+      status = 'Winner is : ' + winner;
+    } else {
+      status = 'The next play is : ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    let moves = history.map((step, move) => {
+      let desc = move ? 'Go to step #' + move : 'Go to first game';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    });
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board valProps2={squares} funcProps2={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
 
+function judgeWinner(squares) {
+  let lines = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8], 
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    let [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 
